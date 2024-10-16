7b2c437de6b2b4b49e8fbf78c1951523d74bf80f
sgkozen <rikasean@gmail.com>
2024-10-16 14:56:06 +0900

    Input, Modal, Map Done

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import './NewPlace.css';
+import Input from '../../shared/components/FormElements/Input';
+
 function NewPlace() {
   return (
-    <div>NewPlace</div>
+    <form className='place-form'>
+      <Input type="text" label="Title" validators={[]} errorText="Please enter a valid title." /*onInput={}*//>
+    </form>
   )
 }
 
