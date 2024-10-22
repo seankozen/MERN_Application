d4ad7ded81c4ed82fe3a44e2ff845dfe61fb9778
sgkozen <rikasean@gmail.com>
2024-10-22 14:55:01 +0900

    Error handling

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -1,5 +1,6 @@
 const HttpError = require("../models/http-error");
-
+const {v4:uuidv4} = require('uuid');
+const uuid = uuidv4();
 
 const DUMMY_PLACES = [
   {
@@ -44,5 +45,21 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+
+  const createdPlace = {
+    id: uuid,
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+  DUMMY_PLACES.push(createdPlace);
+  res.status(201).json({place: createdPlace});
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
