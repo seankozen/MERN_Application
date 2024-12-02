25c668dc5b6758a376e5dbef30d863315c1fecc9
sgkozen <rikasean@gmail.com>
2024-12-02 16:57:33 +0900

    To getByUserId

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -19,17 +19,22 @@ let DUMMY_PLACES = [
   },
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
+  let place;
 
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError('Something went wrong, could not find a place.', 500);
+    return next(error);
   }
 
-  res.json({ place });
+  if (!place) {
+    const error =  new HttpError("Could not find a place for the provided id.", 404);
+    return next(error);
+  }
+  res.json({ place: place.toObject({getters: true}) });
 };
 
 const getPlacesByUserId = (req, res, next) => {
@@ -82,10 +87,6 @@ const createPlace = async (req, res, next) => {
     const error = new HttpError("Creating place failed, please try again", 500);
     return next(error);
   }
-  
-
-
-
   res.status(201).json({ place: createdPlace });
 };
 
